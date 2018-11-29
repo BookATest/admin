f029e5a4ea658e4e530bd1f5c9b526a79a44019e
Matthew Inamdar <matthew.inamdar@goldspecdigital.com>
2018-11-29 10:06:36 +0000

    installed vue-authenticate

--- src/main.js
+++ src/main.js
@@ -4,8 +4,19 @@ import router from '@/router';
 import store from '@/store';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import VueAuthenticate from 'vue-authenticate';
 
 Vue.use(VueAxios, axios);
+Vue.use(VueAuthenticate, {
+  baseUrl: process.env.VUE_APP_API_URI, // Your API domain
+
+  providers: {
+    github: {
+      clientId: process.env.VUA_APP_OAUTH_CLIENT_ID,
+      redirectUri: 'http://localhost:8080/auth/callback', // Your client app URL
+    },
+  },
+});
 
 Vue.config.productionTip = false;
 
