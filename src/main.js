0ef9a3192004a489a49d5185fd431dac7f98952d
Matthew Inamdar <matthew.inamdar@goldspecdigital.com>
2018-11-29 10:23:27 +0000

    split auth into separate file

--- src/main.js
+++ src/main.js
@@ -1,22 +1,7 @@
 import Vue from 'vue';
-import App from '@/App.vue';
-import router from '@/router';
-import store from '@/store';
-import axios from 'axios';
-import VueAxios from 'vue-axios';
-import VueAuthenticate from 'vue-authenticate';
-
-Vue.use(VueAxios, axios);
-Vue.use(VueAuthenticate, {
-  baseUrl: process.env.VUE_APP_API_URI, // Your API domain
-
-  providers: {
-    github: {
-      clientId: process.env.VUA_APP_OAUTH_CLIENT_ID,
-      redirectUri: 'http://localhost:8080/auth/callback', // Your client app URL
-    },
-  },
-});
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
 
--- src/store.js
+++ src/store.js
@@ -1,16 +1,45 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import auth from './auth';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    //
+  },
+  getters: {
+    isAuthenticated() {
+      return auth.isAuthenticated();
+    },
   },
   mutations: {
-
+    //
   },
   actions: {
+    /**
+     * Login logic.
+     * @param {object} context
+     * @param {object} payload
+     */
+    login(context, payload) {
+      auth.login(payload.user, payload.requestOptions).then(() => {
+        context.commit('isAuthenticated', {
+          isAuthenticated: auth.isAuthenticated(),
+        });
+      });
+    },
 
+    /**
+     * Logout logic.
+     * @param {object} context
+     */
+    logout(context) {
+      auth.logout({ url: '/auth/logout' }).then(() => {
+        context.commit('isAuthenticated', {
+          isAuthenticated: auth.isAuthenticated(),
+        });
+      });
+    },
   },
 });
