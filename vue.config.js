325983224c9d817f46a8e457147904f732aa377a
Matthew Inamdar <matthew.inamdar@goldspecdigital.com>
2018-11-29 15:12:38 +0000

    implemented travis

---
+++ vue.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  baseUrl: '/',
+  outputDir: 'dist',
+  indexPath: 'index.html',
+  filenameHashing: true,
+  lintOnSave: true,
+  productionSourceMap: true,
+  pluginOptions: {
+    s3Deploy: {
+      awsProfile: 'default',
+      region: 'eu-west-1',
+      bucket: undefined,
+      assetPath: 'dist',
+      pwa: false,
+      enableCloudfront: true,
+      cloudfrontId: undefined,
+      cloudfrontMatchers: '/*',
+      uploadConcurrency: 5,
+      pluginVersion: '3.0.0',
+      registry: undefined,
+      createBucket: false,
+      staticHosting: false,
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+    },
+  },
+};
