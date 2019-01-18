d24fd998c595ea63ebb86785a70e4f27f9f1a5bd
Matt Inamdar <matt@ayup.agency>
2019-01-18 14:24:30 +0000

    Added most fields to user create

--- src/views/users/Create.vue
+++ src/views/users/Create.vue
@@ -1,5 +1,89 @@
 <template>
-  <button @click="userForm.post('/test')">Click</button>
+  <div class="main">
+
+    <!-- Edit User -->
+    <div class="edit-user">
+
+      <div class="edit-user__intro">
+        <div>
+          <h2>Create User <i class="icon icon--edit"></i></h2>
+        </div>
+      </div>
+
+      <div class="edit-user__details">
+        <div>
+          <form class="form form--edit-user">
+
+            <div class="form__text">
+              <label for="first_name"><span>First name</span></label>
+              <div>
+                  <input v-model="userForm.first_name" type="text" id="first_name" name="first_name">
+              </div>
+            </div>
+
+            <div class="form__text">
+              <label for="last_name"><span>Last name</span></label>
+              <div>
+                  <input v-model="userForm.last_name" type="text" id="last_name" name="last_name">
+              </div>
+            </div>
+
+            <div class="form__text">
+              <label for="email"><span>Email</span></label>
+              <div>
+                  <input v-model="userForm.email" type="email" id="email" name="email">
+              </div>
+            </div>
+
+            <div class="form__text">
+              <label for="phone"><span>Phone</span></label>
+              <div>
+                  <input v-model="userForm.phone" type="tel" id="phone" name="phone">
+              </div>
+            </div>
+
+            <div class="form__text">
+              <label for="password"><span>Password</span></label>
+              <div>
+                  <input v-model="userForm.password" type="password" id="password" name="password">
+              </div>
+            </div>
+
+          </form>
+        </div>
+      </div>
+
+      <div class="edit-user__notification">
+        <h2>Display your email to service users?</h2>
+        <label class="button button__radio"><input v-model="userForm.display_email" name="display_email" type="radio" :value="true"><span>Yes<span></span></span></label>
+        <label class="button button__radio"><input v-model="userForm.display_email" name="display_email" type="radio" :value="false"><span>No</span></label>
+      </div>
+
+      <div class="edit-user__notification">
+        <h2>Display your phone to service users?</h2>
+        <label class="button button__radio"><input v-model="userForm.display_phone" name="display_phone" type="radio" :value="true"><span>Yes<span></span></span></label>
+        <label class="button button__radio"><input v-model="userForm.display_phone" name="display_phone" type="radio" :value="false"><span>No</span></label>
+      </div>
+
+      <div class="edit-user__notification">
+        <h2>Received cancellation confirmations?</h2>
+        <label class="button button__radio"><input v-model="userForm.receive_booking_confirmations" name="receive_booking_confirmations" type="radio" :value="true"><span>Yes<span></span></span></label>
+        <label class="button button__radio"><input v-model="userForm.receive_booking_confirmations" name="receive_booking_confirmations" type="radio" :value="false"><span>No</span></label>
+      </div>
+
+      <div class="edit-user__notification">
+        <h2>Received cancellation confirmations?</h2>
+        <label class="button button__radio"><input v-model="userForm.receive_cancellation_confirmations" name="receive_cancellation_confirmations" type="radio" :value="true"><span>Yes<span></span></span></label>
+        <label class="button button__radio"><input v-model="userForm.receive_cancellation_confirmations" name="receive_cancellation_confirmations" type="radio" :value="false"><span>No</span></label>
+      </div>
+
+      <div class="edit-user__action">
+        <button class="button button__primary button__primary--b"><span>Save</span></button>
+      </div>
+
+    </div>
+
+  </div>
 </template>
 
 <script>
