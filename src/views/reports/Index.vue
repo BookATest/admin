e36a40438e3b3f4eb1c23ee5718901d0ebcf02e0
Matt Inamdar <matt@ayup.agency>
2019-02-08 10:25:04 +0000

    Added empty reports page

---
+++ src/views/reports/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>lorem</div>
+</template>
+
+<script>
+export default {
+  name: 'ReportsIndexView',
+
+  metaInfo() {
+    return {
+      title: this.$route.meta.title,
+    };
+  },
+};
+</script>
