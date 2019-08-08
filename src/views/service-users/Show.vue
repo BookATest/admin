5b9c1a2b0c0372da4f584cbd007e4cb058b7c3f6
Matt Inamdar <matt@ayup.agency>
2019-08-08 13:41:35 +0100

    Service user appointments now loaded

--- src/views/service-users/Show.vue
+++ src/views/service-users/Show.vue
@@ -13,86 +13,72 @@
         </router-link>
       </div>
 
-      <!-- Data -->
-      <div class="service-users__data">
-        <div>
-          <ul>
-            <li><span>Unique ID</span><span>87209842</span></li>
-            <li><span>Name</span><span>Robert Kirk</span></li>
-            <li><span>Phone</span><span>-</span></li>
-            <li><span>Email</span><span>robert.kirk@gmail.com</span></li>
-          </ul>
+      <bat-loader v-if="loading" />
 
-          <form>
-            <div class="form__text">
-              <label>
-                <span>Send feedback submission<br>notification emails?</span>
-              </label>
+      <template v-else>
+        <!-- Data -->
+        <div class="service-users__data">
+          <div>
+            <ul>
+              <li><span>Name</span><span>{{ serviceUser.name }}</span></li>
+              <li><span>Phone</span><span>{{ serviceUser.phone }}</span></li>
+              <li><span>Email</span><span>{{ serviceUser.email || '-' }}</span></li>
+            </ul>
+          </div>
 
-              <label class="button button__radio button__radio">
-                <input name="group" type="radio" disabled>
-                <span>phone</span>
-              </label>
-
-              <label class="button button__radio button__radio">
-                <input checked="checked" name="group" type="radio" disabled>
-                <span>email</span>
-              </label>
+          <div>
+            <div class="card card__stats--patient">
+              <span><strong>{{ dnaRate }}%</strong>DNA rate</span>
             </div>
-          </form>
-        </div>
-
-        <div>
-          <div class="card card__stats--patient">
-            <span><strong>10%</strong>DNA rate</span>
           </div>
         </div>
-      </div>
 
-      <!-- Table -->
-      <div class="service-users__table">
-        <table class="table table--3col" cellpadding="16">
-          <tbody>
-            <tr>
-              <th><span>Date</span></th>
-              <th><span>Location</span></th>
-              <th><span>Feedback</span></th>
-            </tr>
-            <tr>
-              <td><span>18/02/18</span></td>
-              <td><span>Leeds</span></td>
-              <td>
-                <button class="button button__primary button__primary--b" onclick="location.href='../users/user--view.html'">
-                  <span>View</span>
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td><span>18/02/18</span></td>
-              <td><span>Leeds</span></td>
-              <td>
-                <button class="button button__primary button__primary--b" onclick="location.href='../users/user--view.html'">
-                  <span>View</span>
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td><span>18/02/18</span></td>
-              <td><span>Bradford</span></td>
-              <td>
-                <button class="button button__primary button__primary--b" onclick="location.href='../users/user--view.html'">
-                  <span>View</span>
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+        <!-- Table -->
+        <div class="service-users__table">
+          <table class="table table--3col" cellpadding="16">
+            <thead>
+              <tr>
+                <th><span>Date</span></th>
+                <th><span>Location</span></th>
+                <th><span>Feedback</span></th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr
+                v-for="(appointment, index) in appointments"
+                :key="`service-users::show::appointment::${index}`"
+              >
+                <td><span>{{ appointment.start_at }}</span></td>
+                <td><span>{{ appointment.clinic_id }}</span></td>
+                <td>
+                  <router-link
+                    tag="button"
+                    :to="{
+                      name: 'appointments.show',
+                      params: { appointment: appointment.id },
+                    }"
+                    class="button button__primary button__primary--b"
+                  >
+                    <span>View</span>
+                  </router-link>
+                </td>
+              </tr>
+
+              <tr v-if="appointments.length === 0">
+                <td colspan="3">No appointments.</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </template>
     </div>
   </div>
 </template>
 
 <script>
+import BatLoader from '@/components/Loader.vue';
+
 export default {
   name: 'ServiceUsersShowView',
 
@@ -100,6 +86,65 @@ export default {
     return {
       title: this.$route.meta.title,
     };
+  },
+
+  components: { BatLoader },
+
+  data() {
+    return {
+      loadingServiceUser: false,
+      serviceUser: null,
+      loadingAppointments: false,
+      appointments: [],
+    };
+  },
+
+  computed: {
+    loading() {
+      return this.loadingServiceUser || this.loadingAppointments;
+    },
+
+    dnaRate() {
+      const allAppointments = this.appointments.length;
+      const unattendedAppointments = this.appointments.filter(appointment => appointment.did_not_attend === true).length;
+
+      if (unattendedAppointments === 0) {
+        return allAppointments;
+      }
+
+      return Math.round((unattendedAppointments / allAppointments) * 100);
+    },
+  },
+
+  methods: {
+    // Fetch the service user.
+    async fetchServiceUser() {
+      this.loadingServiceUser = true;
+
+      const {
+        data: { data: serviceUser },
+      } = await this.$http.get(`/service-users/${this.$route.params.service_user}`);
+
+      this.serviceUser = serviceUser;
+
+      this.loadingServiceUser = false;
+    },
+
+    // Fetch the appointments for the service user.
+    async fetchAppointments() {
+      this.loadingAppointments = true;
+
+      this.appointments = await this.fetchAll('/appointments', {
+        'filter[service_user_id]': this.serviceUser.id,
+      });
+
+      this.loadingAppointments = false;
+    },
+  },
+
+  async created() {
+    await this.fetchServiceUser();
+    await this.fetchAppointments();
   },
 };
 </script>
